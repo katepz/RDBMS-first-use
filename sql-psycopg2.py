5c4894a2a1316853dac6203de0124544ac57949d
Kate Eaves <3br90lcz@students.codeinstitute.net>
2024-11-06 09:50:08 +0000

    update: name of python file

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -7,6 +7,10 @@ connection = psycopg2.connect(database="chinook")
 #build a cursor object of the database
 cursor = connection.cursor()
 
+#Query 1 - selact all records from the 'Artist' table
+cursor.execute('SELECT * FROM "Artist"')
+
+
 #fetch the results - multiple
 results = cursor.fetchall()
 
