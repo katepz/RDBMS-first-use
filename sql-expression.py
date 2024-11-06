588b1fc0d2111b257cc697e1897f9f2d6044d7ec
Kate Eaves <3br90lcz@students.codeinstitute.net>
2024-11-06 15:34:26 +0000

    Save: updates and new files from coding coach session

---
+++ sql-expression.py
@@ -0,0 +1,14 @@
+from sqlalchemy import (
+    create_engine, Table, Column, Float, ForeignKey, Integer, String, MetaData
+)
+
+#executing the instructions from our localhost "chinook" db
+db = create_engine("postgresql:///chinook")
+
+meta = MetaData(db)
+
+#create variable for "Artist" table
+artist_table = Table(
+    "Artist", meta,
+    
+)
